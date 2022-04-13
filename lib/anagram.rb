b20cd5a7c9a005e07c41165f8dbbe8bfe6871a51
Joe <hutcheson2001@yahoo.com>
2022-04-13 16:54:05 -0400

    commit tree c1eb8a40d82f86f391c13e3282d1149e283ba05e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  attr_accessor :array
+  attr_accessor :anagrams
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match array
+    @array = array
+    new_array = array.map{|w| w.split("")}
+    @anagrams = new_array.filter do |word_array| 
+      word_array.sort == @word.split("").sort
+    end
+    joined_anagrams = @anagrams.map {|w| w.join}
+  end
+end
+
